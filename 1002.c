cb850d2a6e15e2807693a0f0946846cf3ef9516b
Rabeya Noor <rabeyanoor996@gmail.com>
2024-08-20 00:40:37 +0600

    kkk

---
+++ 1002.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+    {
+        double pi=3.14159,r,A;
+        scanf("%lf",&r);
+        A=pi* r*r;
+        printf("A=%.4lf\n",A);
+        return 0;
+    }        
---
+++ 1004.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+    {
+        int x,y,A;
+        scanf("%d %d",&x,&y);
+        A=x*y;  
+        printf("PROD = %d\n",A);
+        return 0;
+    }        
